904b5506c124aa762c3b25d871c5c5182bc4aa51
Pavun786 <pulsarpavuns786@gmail.com>
2024-07-25 11:50:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const DbConnection = require("./Connection/Db.js")
+const propertyRoute = require("./routes/propertyRoute.js")
+const userRoute = require("./routes/userRoute.js")
+
+
+const app = express();
+
+app.use(express.json())
+app.use(cors("*"))
+
+const port = process.env.PORT || 4600;
+
+DbConnection()
+
+app.use('/property',propertyRoute)
+app.use('/user',userRoute)
+
+
+app.get("/",(req,res)=>{
+   res.send("Please welcome ")
+})
+
+app.listen(port,()=>{
+    console.log(`server run on port ${port}`)
+})
